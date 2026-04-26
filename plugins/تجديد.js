7c5cd684d23c02c5f4f9912543522b115c8e88f8
Loydsumer <mohmmug909@gmail.com>
2026-04-26 19:35:52 +0200

    Delete plugins directory

--- plugins/تجديد.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m, {conn}) => {
-  const revoke = await conn.groupRevokeInvite(m.chat);
-  await conn.reply(m.chat, `🔹️ *_تم تغير لينك الجروب._*\n*♾ • اللينك الجديد:* ${'https://chat.whatsapp.com/' + revoke}`, m);
-};
-handler.command = ['resetlink', 'تغيير_اللينك', 'رستر', 'تجديد'];
-handler.botAdmin = true;
-handler.admin = true;
-handler.group = true;
-export default handler;
